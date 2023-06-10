3c673ad9c1003f5d722c540a6e5598c97357e936
NOHHYUNGBIN <shgudqlsz@naver.com>
2023-06-10 19:14:10 +0900

    navbar,router구현, firebase로 구글Oauth login,logout구현

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { logOut, login, onUserStateChange } from "../api/firebase";
+import User from "./User";
+
+export default function Navbar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-2 font-semibold">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>BeenSoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">Carts</Link>
+        <Link to="/products/new" className="text-2xl">
+          <BsFillPencilFill />
+        </Link>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logOut}>LogOut</button>}
+      </nav>
+    </header>
+  );
+}
