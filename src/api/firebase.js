0181de0ac1bce613aef46200bc58e7a374a5ea1c
NOHHYUNGBIN <shgudqlsz@naver.com>
2023-06-18 16:11:49 +0900

    오타수정

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -10,7 +10,7 @@ import {
 import { getDatabase, ref, get, set, remove } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KRY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
